ff0019d7a0bc7de0f5c800275ecad9b59e85f819
Patti85 <pbeier24@googlemail.com>
2020-11-18 22:16:42 +0100

    namespace corrected

--- Models/MenuItem.cs
+++ Models/MenuItem.cs
@@ -9,6 +9,6 @@ namespace article_manager_wasm.Models
     public class MenuItem
     {
         public string Caption { get; set; }
-        public bool Active { get; set; }
+        public string Href { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using countdown_wasm;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace article_manager_wasm
