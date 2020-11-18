f9fa3065200d69dad204000c245447dddeac5617
Patti85 <pbeier24@googlemail.com>
2020-11-18 16:04:37 +0100

    Article Overview Page

---
+++ Models/MenuItem.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace article_manager_wasm.Models
+{
+    public class MenuItem
+    {
+        public string Caption { get; set; }
+        public bool Active { get; set; }
+    }
+}
